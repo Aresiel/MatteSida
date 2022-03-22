c26510296c9b7db032c7eec458317b5cd6822600
Love Dittlau <love@dittlau.se>
2022-03-22 16:11:49 +0100

    Responsive navbar

--- mattesida/src/components/navbar/navbar.jsx
+++ mattesida/src/components/navbar/navbar.jsx
@@ -2,16 +2,24 @@ import {createSignal} from "solid-js";
 import './style.css'
 
 const [pages, setPages] = createSignal([
-    { name: "iρΣμm", path: "index.html"},
     { name: "Roliga Formler", path: "formler.html"},
     { name: "Intressant Teori", path: "teori.html"},
     { name: "Apodiktiska Bevis", path: "bevis.html"},
     { name: "Kontakt", path: "kontakt.html"}
 ])
 
+const toggleMenu = () => {
+    let ul = document.querySelector("nav > ul")
+    ul.classList.toggle("hidden")
+}
+
 export default () =>
     <nav>
-        <ul>
+        <ul className={"hidden"}>
+            <li>
+                <a onClick={toggleMenu}>☰</a>
+                <a href="index.html">iρΣμm</a>
+            </li>
             <For each={pages()}>{(page, i)=>
                 <li>
                     <a href={page.path}>{page.name}</a>
