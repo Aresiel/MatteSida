5cff992f248a7d244d099f5e15d44d0a69db7b4c
Love Dittlau <love@dittlau.se>
2022-04-26 16:12:01 +0200

    changes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-nav pills>
+      <b-nav-item href="/">iρΣμm</b-nav-item>
+      <b-nav-item href="/formler">Roliga Formler</b-nav-item>
+      <b-nav-item href="/testpage">Intressant Teori</b-nav-item>
+      <b-nav-item href="/testpage">Apodiktiska Bevis</b-nav-item>
+      <b-nav-item href="testpage">Kontakt</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<style lang="css">
+  .nav {
+    background-color: #7D7C9D;
+    font-family: 'Ropa Sans', sans-serif;
+    font-size: 1.5rem;
+  }
+
+  a, a:hover {
+    color: #EAEAEA;
+  }
+
+  .nav-item:hover {
+    background-color: rgba(0,0,0,0.11)
+  }
+
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar/>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Cutive&family=Roboto&family=Ropa+Sans&display=swap');
+/*
+    font-family: 'Cutive', serif;
+    font-family: 'Roboto', sans-serif;
+    font-family: 'Ropa Sans', sans-serif;
+ */
+</style>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
